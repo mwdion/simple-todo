8e7b96158b885f8c46de426d3fcc914af1cb42f5
Michael Dion <mwdion@gmail.com>
2015-01-28 09:24:18 -0500

    DRYed todo_items with before action and form partial

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,14 +1,12 @@
 class TodoItemsController < ApplicationController
-  def index
-    @todo_list = TodoList.find(params[:todo_list_id])
+  before_action :find_todo_list
+  def index 
   end
   def new
-    @todo_list = TodoList.find(params[:todo_list_id])
     @todo_item = @todo_list.todo_items.new
   end
 
   def create
-    @todo_list = TodoList.find(params[:todo_list_id])
     @todo_item = @todo_list.todo_items.new(todo_item_params)
     if @todo_item.save
       flash[:success] = "Added todo list item."
@@ -20,12 +18,10 @@ class TodoItemsController < ApplicationController
   end
 
   def edit
-    @todo_list = TodoList.find(params[:todo_list_id])
     @todo_item = @todo_list.todo_items.find(params[:id])
   end
 
   def update
-    @todo_list = TodoList.find(params[:todo_list_id])
     @todo_item = @todo_list.todo_items.find(params[:id])
     if @todo_item.update_attributes(todo_item_params)
       flash[:success] = "Saved todo list item."
@@ -42,6 +38,9 @@ class TodoItemsController < ApplicationController
   end
 
   private
+  def find_todo_list
+    @todo_list = TodoList.find(params[:todo_list_id])
+  end
   def todo_item_params
     params[:todo_item].permit(:content)
   end
